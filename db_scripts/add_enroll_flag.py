7fce55c076e3a6d8f4c7ac9e03fe0e79f9d73ba2
kimberlychelsie <kimberlychelsiemamaril@gmail.com>
2026-03-20 13:23:14 +0800

    Refined Grading Weights Setup and Teacher Class Record display precision

--- db_scripts/add_enroll_flag.py
+++
@@ -1,7 +0,0 @@
-from db import get_db_connection
-
-conn = get_db_connection()
-with conn.cursor() as cur:
-    cur.execute("ALTER TABLE enrollments ADD COLUMN IF NOT EXISTS can_enroll_continuing BOOLEAN DEFAULT false;")
-conn.commit()
-print("Column added")
